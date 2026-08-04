3b4b0bcfbc085b3472612d046bb9c668e44b124a
rich_beluga <znullv2@gmail.com>
2026-08-04 14:20:52 +0300

    refactor(fonts): use static font weights instead of variable font

--- app/src/main/java/com/rich_beluga/richnote/ui/AppFonts.kt
+++ app/src/main/java/com/rich_beluga/richnote/ui/AppFonts.kt
@@ -12,8 +12,20 @@ import com.rich_beluga.richnote.R
  */
 
 val JetBrainsMono = FontFamily(
-    Font(R.font.jetbrains_mono_variable, weight = FontWeight.Normal),
-    Font(R.font.jetbrains_mono_variable, weight = FontWeight.Medium),
-    Font(R.font.jetbrains_mono_variable, weight = FontWeight.Bold),
-    Font(R.font.jetbrains_mono_variable, weight = FontWeight.ExtraBold),
+    Font(
+        R.font.jetbrains_mono_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        R.font.jetbrains_mono_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        R.font.jetbrains_mono_bold,
+        weight = FontWeight.Bold
+    ),
+    Font(
+        R.font.jetbrains_mono_extrabolf,
+        weight = FontWeight.ExtraBold
+    ),
 )
