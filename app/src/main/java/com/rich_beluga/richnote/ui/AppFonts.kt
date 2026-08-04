1de64f401716a1f3819452f44c625cf7f691d44b
rich_beluga <znullv2@gmail.com>
2026-08-04 13:46:26 +0300

    fix(fonts): fix variable font registration

--- app/src/main/java/com/rich_beluga/richnote/ui/AppFonts.kt
+++ app/src/main/java/com/rich_beluga/richnote/ui/AppFonts.kt
@@ -6,14 +6,14 @@ import androidx.compose.ui.text.font.FontWeight
 import com.rich_beluga.richnote.R
 
 /**
- * JetBrains Mono, вариативный шрифт
- * регистрируется несколько раз с разными весами (`Normal`, `Medium`,
- * `Bold` и `ExtraBold`
+ * JetBrains Mono, вариативный шрифт.
+ * Регистрируется несколько раз с разными весами:
+ * `Normal`, `Medium`, `Bold` и `ExtraBold`.
  */
 
 val JetBrainsMono = FontFamily(
-    Font(r.font.jetbrains_mono_variable, weight = FontWeight.Normal)
-    Font(r.font.jetbrains_mono_variable, weight = FontWeight.Medium)
-    Font(r.font.jetbrains_mono_variable, weight = FontWeight.Bold)
-    Font(r.font.jetbrains_mono_variable, weight = FontWeight.ExtraBold)
+    Font(R.font.jetbrains_mono_variable, weight = FontWeight.Normal),
+    Font(R.font.jetbrains_mono_variable, weight = FontWeight.Medium),
+    Font(R.font.jetbrains_mono_variable, weight = FontWeight.Bold),
+    Font(R.font.jetbrains_mono_variable, weight = FontWeight.ExtraBold),
 )
